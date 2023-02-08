0fcc96ec257118b19cc89741e00e11f330fae003
Fakhar Abbas <afakhar363@gmail.com>
2023-02-09 00:21:45 +0500

    First Commit

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+// Composables
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    redirect: "/detail",
+    component: () => import("@/views/Home.vue"),
+    children: [
+      {
+        path: "/detail",
+        name: "detail",
+        component: () => import("@/views/DetailPage.vue"),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/DetailPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <detail-part></detail-part>
+</template>
+
+<script>
+import DetailPart from "@/components/DetailPart.vue";
+export default {
+  components: { DetailPart },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
